b77ee1b1a6352fb12c05e13c3a1d82aba96f85d9
Shahid Hussain <shahid56tk@hotmail.com>
2022-10-04 21:48:47 +0500

    flatlist for messages implemented

--- src/screens/chatScreen/index.js
+++ src/screens/chatScreen/index.js
@@ -1,13 +1,84 @@
 import { useState } from "react"
-import { Button, TextInput, View, StyleSheet, TouchableOpacity, Text } from "react-native"
-import { COLOR_BACKGROUND, COLOR_SECONDARY_TEXT, COLOR_PRIMARY, COLOR_BTN_TEXT, COLOR_ACCENT, COLOR_DIVIDER, COLOR_DARK_PRIMARY, COLOR_PRIMARY_TEXT } from "../../../res/drawables/colors"
-
+import { Button, TextInput, View, StyleSheet, TouchableOpacity, Text, FlatList } from "react-native"
+import { COLOR_BACKGROUND, COLOR_SECONDARY_TEXT, COLOR_PRIMARY, COLOR_BTN_TEXT, COLOR_ACCENT, COLOR_DIVIDER, COLOR_DARK_PRIMARY, COLOR_PRIMARY_TEXT, COLOR_MY_MSG } from "../../../res/drawables/colors"
+const DATA = [
+    {
+      username: 'Qasim',
+      msg: 'Aoa',
+    },
+    {
+      username: 'Majid',
+      msg: 'Wa',
+    },
+    {
+      username: 'Waqas',
+      msg: 'How r u?',
+    },
+    {
+        username: 'Qasim',
+        msg: 'Aoa',
+      },
+      {
+        username: 'Majid',
+        msg: 'Wa',
+      },
+      {
+        username: 'Waqas',
+        msg: 'How r u?',
+      },
+      {
+        username: 'Qasim',
+        msg: 'Aoa',
+      },
+      {
+        username: 'Majid',
+        msg: 'Wa',
+      },
+      {
+        username: 'Waqas',
+        msg: 'How r u?',
+      },
+      {
+          username: 'Qasim',
+          msg: 'Aoa',
+        },
+        {
+          username: 'Majid',
+          msg: 'Wa',
+        },
+        {
+          username: 'Waqas',
+          msg: 'How r u?',
+        },
+  ];
+  
 const Chat = (props)=>{
 
     const {userName} = props.route.params
     const [message, setMessage] = useState(null)
+
+
+      const renderItem = ({ item }) => (
+        item.username==userName?<TouchableOpacity onLongPress={()=> alert(item.msg)}>
+            <View style= {styles.listItemMe}>
+                <Text style= {{...styles.msgText, fontWeight:'bold'}}>Me</Text>
+                <Text style= {styles.msgText}>{item.msg}</Text>
+            </View>
+        </TouchableOpacity>: 
+        <TouchableOpacity onLongPress={()=> alert(item.msg)}>
+            <View style= {styles.listItem}>
+            <Text style= {{...styles.msgText, fontWeight:'bold'}}>{item.username}</Text>
+            <Text style= {styles.msgText}>{item.msg}</Text>
+        </View>
+        </TouchableOpacity>
+      );
     return(
         <View style= {styles.container}>
+            <FlatList style= {{marginTop:15}}
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
             <View style = {styles.chatEditor}>
                 <TextInput
                     style= {styles.chatInput}
@@ -32,15 +103,13 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor: COLOR_BACKGROUND,
-        alignItems:'center',
-        justifyContent:'center'
+        justifyContent:'space-between'
     },
     chatEditor:{
         flexDirection:'row',
         width:'98%',
         justifyContent:'center',
-        position:'absolute',
-        bottom:17,
+        marginBottom:10
     },
     chatInput:{
         width:'78%',
@@ -67,6 +136,25 @@ const styles = StyleSheet.create({
         fontSize:23,
         textAlign:'center',
         textAlignVertical:'center'
+    },
+    listItem:{
+        backgroundColor: COLOR_ACCENT,
+        margin:10,
+        padding:10,
+        width:'60%',
+        borderRadius:10,
+    },
+    listItemMe:{
+        backgroundColor: COLOR_MY_MSG,
+        margin:10,
+        padding:10,
+        width:'60%',
+        borderRadius:10,
+        alignSelf:'flex-end',
+        textAlign:'left'
+    },
+    msgText:{
+        fontSize:14,
     }
 })
 export default Chat;
