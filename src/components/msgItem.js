d449e75933476668bc57daef085e9acaf4030f48
Shahid Hussain <shahid56tk@hotmail.com>
2022-10-04 22:23:48 +0500

    component

---
+++ src/components/msgItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const MsgItem = (props) =>{
+    return(
+        <TouchableOpacity onLongPress={props.onPress}>
+            <View style= {{...props.style, ...styles.item}}>
+                <Text style= {{...props.msgText, fontWeight:'bold'}}>{props.username}</Text>
+                <Text style= {styles.txt}>{props.msg}</Text>
+            </View>
+        </TouchableOpacity>
+        
+    )
+};
+
+const styles = StyleSheet.create({
+    item:{
+        margin:10,
+        padding:10,
+        width:'70%',
+        borderRadius:10,
+    },
+    txt:{
+        //fontSize:12,
+    }
+})
+export default MsgItem
